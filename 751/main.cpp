e091b1d12df33336a43ffa4bb587dfb2f075bfe2
Fed <punnaphop.sitthi@gmail.com>
2022-10-10 20:29:19 +0700

    🔄 redo question 751

--- 751/main.cpp
+++ 751/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
     ios::sync_with_stdio(false);cin.tie(0);
     string s;cin>>s;
-    int o=0,t=0,g=0,count=0;;
+    int o=0,t=0,g=0,count=0;
     for(int i=0;i<s.size();i++){
         if(s[i]=='O'){
             o++;
