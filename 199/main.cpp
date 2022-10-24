a23baea57ac200431675b077f2963061ed3e0c7a
Fed <punnaphop.sitthi@gmail.com>
2022-10-24 13:57:44 +0700

    ✅ solved problem 199

--- 199/main.cpp
+++ 199/main.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main(){
     ios::sync_with_stdio(false);cin.tie(0);
-    int a,b,gcd,temp;
+    long long int a,b,gcd;
     cin>>a>>b;
-    temp=a*b;
+    long long int temp=a*b;
     while(a*b!=0){
         if(a>b){
             a%=b;
@@ -16,11 +16,9 @@ int main(){
     }
     if(a==0){
         gcd=b;
-    }else if(b==0){
-        gcd=a;
     }else{
-        gcd=0;
+        gcd=a;
     }
-    cout<<(temp/gcd)<<endl;
+    cout<<(temp/gcd)<<"\n";
     return 0;
 }
