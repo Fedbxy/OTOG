2deed8f5fde834201a87a2a94d681adac4efd150
Fed <punnaphop.sitthi@gmail.com>
2022-10-24 12:19:00 +0700

    ✅ solved the problem after the contest

--- Contest#72 ตูเต้ลผจญภัย/829 แผ่นหิน/main.cpp
+++ Contest#72 ตูเต้ลผจญภัย/829 แผ่นหิน/main.cpp
@@ -5,30 +5,24 @@ using namespace std;
 int main(){
     ios::sync_with_stdio(false);cin.tie(0);
     int n,q;cin>>n>>q;
-    long long int a1[n],a2[q];
+    long long int a[n];
+    long long int n_a[n-1];
     for(int i=0;i<n;i++){
-        cin>>a1[i];
+        cin>>a[i];
     }
-    for(int i=0;i<q;i++){
-        cin>>a2[i];
-        for(int j=0;j<n;j++){
-            if(j==0){
-                if(a1[j]+a1[j+1]>=a2[i]){
-                    cout<<j+1<<endl;
-                    break;
-                }
-            }else if(j==n-1){
-                if(a1[j-1]+a1[j]>=a2[i]){
-                    cout<<j+1<<endl;
-                    break;
-                }
-            }else{
-                if(a1[j-1]+a1[j]+a1[j+1]>=a2[i]){
-                    cout<<j+1<<endl;
-                    break;
-                }
-            }
+    for(int i=0;i<n-1;i++){
+        if(i==0){
+            n_a[i]=a[i]+a[i+1];
+        }else if(i==n-1){
+            n_a[i]=a[i-1]+a[i];
+        }else{
+            n_a[i]=a[i-1]+a[i]+a[i+1];
         }
+    }
+    // sort(n_a,n_a+n);
+    for(int i=0;i<q;i++){
+        long long int v;cin>>v;
+        cout<<lower_bound(n_a,n_a+n-1,v)-n_a+1<<"\n";
     }
     return 0;
 }
